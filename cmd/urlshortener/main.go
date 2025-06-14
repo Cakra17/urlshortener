354f7048a18865edbebbe1475ae3d6d73278a877
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-06-14 12:08:36 +0800

    feat: add structured logging

--- cmd/urlshortener/main.go
+++ cmd/urlshortener/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/cakra17/urlshortener/internal/handler"
 	"github.com/cakra17/urlshortener/internal/storage"
@@ -11,11 +12,25 @@ import (
 )
 
 func main() { 
-  db := storage.InitDB("./urls.db")
+  
+  port := os.Getenv("URLSHORT_PORT")
+	if port == "" {
+		port = "6969"
+	}
+
+	dbPath := os.Getenv("URLSHORT_DB_PATH")
+	if dbPath == "" {
+		dbPath = "./urls.db"
+	}
+
+  db := storage.InitDB(dbPath)
   defer db.Close()
+  
+  logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
   server := &handler.Server{
     DB: db,
+    Logger: logger,
   }
 
   mux := http.NewServeMux()
@@ -23,6 +38,6 @@ func main() {
   mux.HandleFunc("POST /shorten", server.ShortenHandler)
   mux.HandleFunc("GET /{code}", server.RedirectHandler)
 
-  fmt.Println("[INFO] Server is running on port 6969")
+  logger.Info("[INFO] Server is running on port 6969", "port", port)
   log.Fatal(http.ListenAndServe(":6969", mux))
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,32 +4,37 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/cakra17/urlshortener/internal/models"
 )
 
 type Server struct {
-  DB *sql.DB
+  DB      *sql.DB
+  Logger  *slog.Logger
 }
 
 func (s *Server) ShortenHandler(w http.ResponseWriter, r *http.Request) {
 	var body models.URL
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+    s.Logger.Error("Failed to decode body", "error", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	stmt, err := s.DB.Prepare("INSERT INTO tb_urls (short_url, long_url) VALUES(?, ?);")
 	if err != nil {
-		http.Error(w, "Failed to Prepare statment", http.StatusInternalServerError)
+    s.Logger.Error("Failed to Prepare statement", "error", err)
+		http.Error(w, "Something wrong with the server", http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(body.ShortURL, body.LongURL)
 	if err != nil {
+    s.Logger.Error("Failed to Insert data", "error", err)
 		http.Error(w, fmt.Sprintf("[ERROR] %s", err.Error()), http.StatusBadRequest)
 		return
 	}
@@ -37,12 +42,14 @@ func (s *Server) ShortenHandler(w http.ResponseWriter, r *http.Request) {
 	response := &models.SuccessRes{Status: "success", ShortURL: body.ShortURL}
 	jsonByte, err := json.Marshal(response)
 	if err != nil {
+    s.Logger.Error("failer to encode to json", "error", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+  s.Logger.Info("url shortener successfully")
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonByte)
+	w.Write(jsonByte) 
 }
 
 func (s *Server) RedirectHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,6 +57,7 @@ func (s *Server) RedirectHandler(w http.ResponseWriter, r *http.Request) {
 
   stmt, err := s.DB.Prepare("SELECT long_url FROM tb_urls WHERE short_url = ?;")
 	if err != nil {
+    s.Logger.Error("Failed to Prepare statement", "error", err)
 		http.Error(w, "Failed to Prepare statment", http.StatusInternalServerError)
 		return
 	}
@@ -58,9 +66,11 @@ func (s *Server) RedirectHandler(w http.ResponseWriter, r *http.Request) {
   var longUrl string
   if err := stmt.QueryRow(p).Scan(&longUrl); err != nil {
     if err == sql.ErrNoRows {
+      s.Logger.Error("Data not found", "error", err)
       http.NotFound(w, r)
       return
     }
+    s.Logger.Error("Database Error", "error", err)
     http.Error(w, "Database Error", http.StatusInternalServerError)
   }
 
